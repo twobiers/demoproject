b622b0c3a8088ffa264cd31fa78697d00d3b657e
Tobias Hund <tobias.hund@protonmail.com>
2020-07-03 07:50:56 +0200

    Fixed Code Coverage to 100% Updated README.md

--- src/main/java/org/example/Enum1.java
+++ src/main/java/org/example/Enum1.java
@@ -1,3 +1,16 @@
 package org.example;
 
-public enum Enum1 {}
+public enum Enum1 {
+  TEST("TEST TEST 123"),
+  TEST2("TEST2 TEST2 321");
+
+  private final String testString;
+
+  Enum1(String s) {
+    testString = s;
+  }
+
+  public String getTestString() {
+    return testString;
+  }
+}
